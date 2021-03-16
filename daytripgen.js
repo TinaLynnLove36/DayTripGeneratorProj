2a8887b9f8bb9b3f28e19406648ea09042e63d2c
TinaLynnLove36 <you@example.com>
2021-03-16 02:29:12 -0500

    Init

---
+++ daytripgen.js
@@ -0,0 +1,39 @@
+let destination = ['Destin', 'Palm Beach', 'Naples', 'St. Augstine', 'Cocoa Beach', 'Miami'];
+let restaurant = ["The Floridian", "Joe's Stone Crab", "Victoria & Albert's", "Cafe Boulud", "Bern's Steakhouse", "Palme d'Or"];
+let modeOfTransportation = ['Limo', 'Commuter Rail', 'Bus Transit', 'Uber', 'Trolley', 'Taxi'];
+let entertainment = ['Sea World Theme Park', 'Universal Studios', 'Walt Disney World', 'Bush Gardens Zoo', 'Everglades National Park', 'Kennedy Space Center'];
+
+function place() {
+    let randomDestination = destination[Math.floor(Math.random() * destination.length)];
+    return randomDestination; 
+
+}
+
+
+
+ function food() {
+    let randomRestaurant = restaurant[Math.floor(Math.random() * restaurant.length)];
+     return randomRestaurant; 
+
+}
+
+ function transport() {
+   let randomModeTransport = modeOfTransportation[Math.floor(Math.random() * modeOfTransportation.length)];
+     return randomModeTransport; 
+}
+
+function entertain() {
+    let randomEntertainment = entertainment[Math.floor(Math.random() * entertainment.length)];
+     return randomEntertainment; 
+
+} 
+
+
+//let dayTimeTransport = transport();
+//let dayTimeEntertain = entertain();
+function result() {
+    document.getElementById('quoteDisplay').innerHTML = (`You will be visiting ${place()} and eating at ${food()} for lunch, one of Florida's Premier top 5 Star restaruants! You will also have the pleasure of enjoying ${entertain()}! Expenses include travel by ${transport()}!`);
+  
+}
+
+
