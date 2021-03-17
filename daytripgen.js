91bdf1602d4aa9c4eb1d074023cd32009f50e04a
TinaLynnLove36 <you@example.com>
2021-03-16 21:47:25 -0500

    Completed work. Added prompts and alerts to ensure generator copies to console.

--- daytripgen.js
+++ daytripgen.js
@@ -2,38 +2,53 @@ let destination = ['Destin', 'Palm Beach', 'Naples', 'St. Augstine', 'Cocoa Beac
 let restaurant = ["The Floridian", "Joe's Stone Crab", "Victoria & Albert's", "Cafe Boulud", "Bern's Steakhouse", "Palme d'Or"];
 let modeOfTransportation = ['Limo', 'Commuter Rail', 'Bus Transit', 'Uber', 'Trolley', 'Taxi'];
 let entertainment = ['Sea World Theme Park', 'Universal Studios', 'Walt Disney World', 'Bush Gardens Zoo', 'Everglades National Park', 'Kennedy Space Center'];
+let randomDayTrip = [];
 
 function place() {
-    let randomDestination = destination[Math.floor(Math.random() * destination.length)];
-    return randomDestination; 
-
+let randomPlace = destination[Math.floor(Math.random() * destination.length)];
+return randomPlace;
 }
 
-
-
- function food() {
-    let randomRestaurant = restaurant[Math.floor(Math.random() * restaurant.length)];
-     return randomRestaurant; 
-
+function food() {
+let randomFood = restaurant[Math.floor(Math.random() * restaurant.length)];
+return randomFood;
 }
 
- function transport() {
-   let randomModeTransport = modeOfTransportation[Math.floor(Math.random() * modeOfTransportation.length)];
-     return randomModeTransport; 
+function transport() {
+    let randomTransport = modeOfTransportation[Math.floor(Math.random() * modeOfTransportation.length)];
+    return randomTransport;
 }
 
 function entertain() {
-    let randomEntertainment = entertainment[Math.floor(Math.random() * entertainment.length)];
-     return randomEntertainment; 
-
-} 
-
-
-//let dayTimeTransport = transport();
-//let dayTimeEntertain = entertain();
-function result() {
-    document.getElementById('quoteDisplay').innerHTML = (`You can visit ${place()} and have lunch at ${food()}, one of Florida's Premier top 5 Star restaurants! You can also travel by ${transport()} and experience ${entertain()}!`);
-  
+    let randomEntertain = entertainment[Math.floor(Math.random() * entertainment.length)];
+    return randomEntertain;
 }
 
+function logDayTrip() {
+   let result = (`You can visit ${place()} and have lunch at ${food()}, one of Florida's Premier top 5 Star restaurants! You can also travel by ${transport()} and experience ${entertain()}!`);
+   document.getElementById('quoteDisplay').innerHTML = result;
+   
+    if (confirm(result)) {
+        alert('Day Trip Selection Complete! View Console Log for Details');
+        console.log(result);
+        
+    }   else {
+        alert('Click Generator below for more Day Trip Ideas');
+       
+        
+    } 
+           
+}
+   
+   
 
+
+
+
+
+
+
+     
+
+      
+ 
